4961a19e3d5c45cf9d8f02aa34d94fa6ca8600e9
Robert Busa-Fekete <busafekete@yahoo-inc.com>
2017-08-16 08:56:11 -0400

    script modified

--- synthetic-test/scripts/run.sh
+++ synthetic-test/scripts/run.sh
@@ -1,2 +1,5 @@
-#java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_sgd_vw.txt
+java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_sgd_vw.txt
 java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_pistol.txt
+java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_svrg.txt
+java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_solo.txt
+java -classpath ../target/yamall-synthetic-test-jar-with-dependencies.jar com.yahoo.labs.yamall.synthetic.TestLearners params_free_rex.txt
